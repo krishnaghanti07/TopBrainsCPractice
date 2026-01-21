c02aec02470a686fffd17507ded5aaca50cd81dc
Srikrishna Ghanti <nandini.n10s05@gmail.com>
2026-01-21 12:34:16 +0530

    Annded new

---
+++ GreatestOfThree/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System ;
+
+public class GreatestOfThree
+{
+    public static void Main (string[] args)
+    {
+        Console.Write("Enter The First Number : ") ;
+        int num1 = int.Parse(Console.ReadLine()) ;
+
+        Console.Write("Enter The Second Number : ") ;
+        int num2 = int.Parse(Console.ReadLine()) ;
+
+        Console.Write("Enter The Third Number : ") ;
+        int num3 = int.Parse(Console.ReadLine()) ;
+
+        int greatest ;
+
+        if (num1 >= num2 && num1 >= num3)
+        {
+            greatest = num1 ;
+        }
+        else if (num2 >= num1 && num2 >= num3)
+        {
+            greatest = num2 ;
+        }
+        else
+        {
+            greatest = num3 ;
+        }
+
+        Console.WriteLine("The Greatest Number is : " + greatest) ;
+    }
+}
