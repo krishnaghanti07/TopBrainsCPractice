780c08eb353487f5ccc69710b220eb285b7a646c
Srikrishna Ghanti <nandini.n10s05@gmail.com>
2026-01-21 14:44:05 +0530

    Modified

--- UnitConversion/Program.cs
+++ UnitConversion/Program.cs
@@ -1,26 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-
-using System ;
+﻿using System;
 
 public class UnitConversion
 {
-    public static void Main (string[] args)
+    public static double FeetToCentimeters(int feet)
     {
-        Console.Write("Enter The Length in Feet (Integer) : ") ;
-        int feet = Convert.ToInt32(Console.ReadLine()) ;
+        double centimeters = feet * 30.48;
+        return Math.Round(centimeters, 2, MidpointRounding.AwayFromZero);
+    }
 
-        if (feet < 0 || feet > int.MaxValue)
-        {
-            Console.WriteLine("Invalid Input") ;
-            return ;
-        }
+    public static void Main(string[] args)
+    {
+        int feet = 10;   
+        double result = FeetToCentimeters(feet);
 
-        double cm = feet * 30.48 ;
-        double res = Math.Round (cm, 2) ;
-
-        Console.WriteLine("Length in Centimeters (rounded): " + res) ;
-
+        Console.WriteLine(result);
     }
 }
